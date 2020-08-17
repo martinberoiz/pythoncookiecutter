0a90ca5161c050ba65bfc25a6f61f3f0becd6ff3
Martin Beroiz <martinberoiz@gmail.com>
2020-08-17 16:42:33 -0500

    Improvements to setup

--- setup.py
+++ setup.py
@@ -1,10 +1,42 @@
 from setuptools import setup
 
+# =============================================================================
+# PATH TO THIS MODULE
+# =============================================================================
+
+PATH = os.path.abspath(os.path.dirname(__file__))
+
+
+# =============================================================================
+# Get the version from the module file itself (not imported)
+# =============================================================================
+
+MODULE_PY_PATH = os.path.join(PATH, "simplemath.py")
+
+with open(MODULE_PY_PATH, 'r') as f:
+    for line in f:
+        if line.startswith('__version__'):
+            _, _, MODULE_VERSION = line.replace('"', "").split()
+            break
+
+
+# =============================================================================
+# RETRIEVE TE README
+# =============================================================================
+
+README_MD_PATH = os.path.join(PATH, "README.md")
+
+with open(README_MD_PATH, 'r') as f:
+    LONG_DESCRIPTION = f.read()
+
+
 setup(name='simplemath',
-      version='1.1',
+      version=MODULE_VERSION,
       description='Simple Math Module',
+      long_description=LONG_DESCRIPTION,
       author='Martin Beroiz',
       author_email='martinberoiz@gmail.com',
       py_modules=['simplemath', ],
+      install_requires=[numpy,],
       test_suite='tests',
      )
